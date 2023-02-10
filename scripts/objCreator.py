6a0e29c3af49d5487bc3b714b4815ebe4dd096a3
py-official <85795337+py-official@users.noreply.github.com>
2023-02-10 18:27:59 +0300

    Update objCreator.py

--- scripts/objCreator.py
+++ scripts/objCreator.py
@@ -1,9 +1,16 @@
-from jsonReader import getData
 import disnake
 
 
+
+
+embed = {
+		"ErrorNo" : ["Successful!!", "Starting server"],
+		"ErrorServerHasAlreadyStarted" :["Error x 1", "Server has already started"],
+		"ErrorNoStop" : ["Successful!!", "Stopping server"]
+	}
+
 def CreateEmbed(code):
-    content = getData("contentData", "embed", code)
+    content = embed[code]
     embedTemp = disnake.Embed(
             title = content[0],
             description = content[1],
